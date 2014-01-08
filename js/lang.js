12dfcac933a018d534e035c9914e265731279ba5
ufo22940268 <ufo22940268@gmail.com>
2014-01-08 09:01:38 +0800

    up

---
+++ js/lang.js
@@ -0,0 +1,18 @@
+// Localized user interface.
+var tr = {
+    "off": "开启",
+    "on": "关闭",
+    "syntax": "语法高亮",
+    "lineno": "行号",
+    "reset": "重置",
+    "format": "Format Source Code",
+    "kill": "Kill Program",
+    "run": "运行",
+    "compile": "Compile and Run",
+    "more": "Options",
+    "toc": "Table of Contents",
+    "prev": "Previous",
+    "next": "Next",
+    "waiting": "等待远程服务器...",
+    "errcomm": "远程服务器异常.",
+};
